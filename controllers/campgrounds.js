8953026fda6e040f5caf5a5d9a86e8a25f063e3a
Ayush <ayushkhanduri2612@gmail.com>
2023-09-17 01:47:29 +0530

    Added Map

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapboxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
@@ -11,15 +14,25 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+
   const images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
   }));
+
   const campground = await Campground.create({
     ...req.body.campground,
+    geometry: geoData.body.features[0].geometry,
     author: req.user._id,
     images,
   });
+
   console.log(campground);
   req.flash("success", "Successfully made a new campground!");
   res.redirect(`/campgrounds/${campground._id}`);
@@ -35,8 +48,17 @@ module.exports.renderEditForm = async (req, res) => {
 };
 
 module.exports.updateCampground = async (req, res) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const id = req.params.id;
-  const data = req.body.campground;
+  const data = {
+    ...req.body.campground,
+    geometry: geoData.body.features[0].geometry,
+  };
   console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, data, {
     runValidators: true,
---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = mapBoxToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/light-v11", // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
+});
+
+new mapboxgl.Marker({ color: "black" })
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${campground.title}</h4><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
